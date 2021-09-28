dbf831e96c6f6bc50c6a4cbf38b03720d9300d8d
CianMW <markwick.cian@gmail.com>
2021-09-28 13:48:05 +0100

    Navbar complete

---
+++ m4-d2/src/components/MyNavBar.jsx
@@ -0,0 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+
+
+const MyNavBar = () => {
+
+    return (
+        <Navbar bg="danger" variant="dark">
+        <Navbar.Brand href="#">SumBooks</Navbar.Brand>
+        <Nav className="justify-content-sm-between ">
+        <Nav.Link href="#">Home</Nav.Link>
+        <Nav.Link href="#">About</Nav.Link>
+        <Nav.Link href="#">Browse</Nav.Link>
+        </Nav>
+        </Navbar>
+    )
+}
+
+export default MyNavBar
