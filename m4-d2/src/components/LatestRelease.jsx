2bb1b4ec45e8556901fd7f2db4a7ee7d187214ca
CianMW <markwick.cian@gmail.com>
2021-09-28 16:37:09 +0100

    formatted

--- m4-d2/src/components/LatestRelease.jsx
+++ m4-d2/src/components/LatestRelease.jsx
@@ -1,40 +1,35 @@
-import books from '../Data/fantasy.json'
-import Container from  'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import { Component } from 'react'
-import Col from 'react-bootstrap/Col'
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
+import books from "../Data/fantasy.json";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import { Component } from "react";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 
 class NewestReleases extends Component {
+  state = {};
 
-    state = {
-
-    }
-
-    render(){
-
-
-        return (
-        <Container fluid>
-            <Row className="justify-content-center">
-                {   books.map(book => (
-                     <Col xs={4} md={2} key={book.asin} >
-                         <Card >
-                            <Card.Img variant="top" style={{objectFit: "cover", width: "100%", height: "vh"}} src={book.img}  />
-                                <Card.Body>
-                                    <Card.Title>{book.title}</Card.Title>
-                                </Card.Body>
-                        </Card>
-                     </Col>
-                )
-
-                )}
-
-            </Row>
-        </Container>
-        )
-    }
+  render() {
+    return (
+      <Container fluid>
+        <Row className="justify-content-center">
+          {books.map((book) => (
+            <Col xs={4} md={2} key={book.asin}>
+              <Card>
+                <Card.Img
+                  variant="top"
+                  style={{ objectFit: "cover", width: "100%", height: "vh" }}
+                  src={book.img}
+                />
+                <Card.Body>
+                  <Card.Title>{book.title}</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    );
+  }
 }
 
-export default NewestReleases
+export default NewestReleases;
